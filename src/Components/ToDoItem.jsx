fea0c576f235a6e26ca737828defca69c86d2eda
haimi <s0118213@stud.uni-frankfurt.de>
2024-06-26 11:21:06 +0200

    Edit function in progress

--- src/Components/ToDoItem.jsx
+++ src/Components/ToDoItem.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./todoitem.css";
 function TodoItem({ task, currentTask, onEdit, deleteTask }) {
     /* function handleChange() {
         toggleCompleted(task.id);
     } */
+const [input, setInput] = useState('');
 
     return (
         <li key={task.id} className="todo__item">
             {/* TODO: Add edit function, get value of input text value and let update func work */}
-            {(currentTask === task.id) ? <input type="text" defaultValue={task.title} onChange={(e)=> {}} required />
+            {(currentTask === task.id) ? <input type="text" defaultValue={task.title} onInput={e => setInput(e.target.value)} required /> 
                 : <p>{task.title}</p>}
-
+            {console.log(input)}
             <p>{task.description}</p>
             <div className='button__container'>
                 <button onClick={() => deleteTask(task.id)}>
                     X
                 </button>
-                <button onClick={() =>  {currentTask ? onEdit(null,[task.title,task.description]) : onEdit(task.id,[])}}>
+                <button onClick={() =>  {currentTask ? onEdit(null,[input,task.description]) : onEdit(task.id,[])}}>
                 {(currentTask === task.id) ? 'Update Task' : 'Add Task'}
                 </button>
             </div>
